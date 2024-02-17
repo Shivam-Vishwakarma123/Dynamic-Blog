2bdfed2861b71ef49638ad4fefbf3eef89ccae93
Shivam-Vishwakarma123 <shiva83023@gmail.com>
2024-02-18 00:22:34 +0530

    Set up the theme and all the pages and images path

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <title>New Vision - Page 2</title>
-    
+
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800" rel="stylesheet" /> <!-- https://fonts.google.com/specimen/Open+Sans?selection.family=Open+Sans -->
     <link href="{{asset('theme/css/all.min.css')}}" rel="stylesheet" /> <!-- https://fontawesome.com/ -->
     <link href="{{asset('theme/slick/slick.css')}}" rel="stylesheet" /> <!-- https://kenwheeler.github.io/slick/ -->
     <link href="{{asset('theme/slick/slick-theme.css')}}" rel="stylesheet" />
     <link href="{{asset('theme/css/bootstrap.min.css')}}" rel="stylesheet" /> <!-- https://getbootstrap.com -->
     <link href="{{asset('theme/css/templatemo-new-vision.css')}}" rel="stylesheet" />
-<!--
+    <!--
 
 New Vision Template
 
@@ -21,6 +22,7 @@ https://templatemo.com/tm-542-new-vision
 
 -->
 </head>
+
 <body>
     <!-- Page Header -->
     <div class="container-fluid">
@@ -32,23 +34,20 @@ https://templatemo.com/tm-542-new-vision
                         <h1 class="tm-site-name">svBlogs</h1>
                     </div>
                     <div class="tm-site-header-right tm-menu-container-outer">
-                        
+
                         <!--Navbar-->
                         <nav class="navbar navbar-expand-lg">
-                        
-                          <!-- Collapse button -->
-                          <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
-                            aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
-                                class="fas fa-bars fa-1x"></i></span></button>
-                        
-                          <!-- Collapsible content -->
-                          <div class="collapse navbar-collapse tm-nav" id="navbarSupportedContent1">
-                        
-                            <!-- Links -->
-                            <ul class="navbar-nav mr-auto">
+
+                            <!-- Collapse button -->
+                            <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i class="fas fa-bars fa-1x"></i></span></button>
+
+                            <!-- Collapsible content -->
+                            <div class="collapse navbar-collapse tm-nav" id="navbarSupportedContent1">
+
+                                <!-- Links -->
+                                <ul class="navbar-nav mr-auto">
                                     <li class="nav-item active">
-                                        <a class="nav-link tm-nav-link" href="{{asset('home')}}">Home <span
-                                                class="sr-only">(current)</span></a>
+                                        <a class="nav-link tm-nav-link" href="{{asset('home')}}">Home <span class="sr-only">(current)</span></a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link tm-nav-link" href="{{asset('company')}}">Our Company</a>
@@ -60,34 +59,34 @@ https://templatemo.com/tm-542-new-vision
                                         <a class="nav-link tm-nav-link" href="{{asset('contact')}}">Contact</a>
                                     </li>
                                 </ul>
-                            <!-- Links -->
-                        
-                          </div>
-                          <!-- Collapsible content -->
-                        
+                                <!-- Links -->
+
+                            </div>
+                            <!-- Collapsible content -->
+
                         </nav>
                         <!--/.Navbar-->
                     </div>
                 </div>
             </div>
         </div>
-        
+
         <div class="row">
             <div class="col-12">
-                <div class="tm-main-bg tm-about-bg"></div>        
+                <div class="tm-main-bg tm-about-bg"></div>
             </div>
         </div>
-        
+
         <!-- Main -->
         <main>
             <!-- Welcome section -->
             <section class="tm-welcome">
                 <div class="row">
                     <div class="col-12">
-                        <h2 class="tm-section-header tm-header-floating">About Our Company</h2>         
+                        <h2 class="tm-section-header tm-header-floating">About Our Company</h2>
                     </div>
                 </div>
-                
+
                 <div class="row tm-welcome-row">
                     <div class="tm-about">
                         <div class="col-12 tm-media tm-media-v-center">
@@ -107,39 +106,39 @@ https://templatemo.com/tm-542-new-vision
                             <div>
                                 <p>Duis accumsan dolor feugiat dapibus ultrices. Vestibulum consectetur urna augue, in sagittis mi sodales ut. Maecenas at quam et ligula vulputate commodo. Integer tempor nec velit ut pulvinar. In hac habitasse platea dictumst.</p>
                             </div>
-                        </div>    
+                        </div>
                     </div>
                 </div>
-                
+
                 <div class="row tm-welcome-row-2">
                     <div class="col-12">
-                        <h2 class="tm-section-header tm-section-header-small mb-4">Our Background and Environment</h2>        
+                        <h2 class="tm-section-header tm-section-header-small mb-4">Our Background and Environment</h2>
                     </div>
                     <div class="col-md-6">
                         <div class="tm-about-1">
                             <img src="img/img-3x1-01.jpg" alt="Image" class="img-fluid mb-5">
                             <p class="tm-article-text">Phasellus blandit tortor vel odio hendrerit, eleifend ultricies odio temport. Etiam euismod, lectus convallis efficitur convallis, risus orci auctor orci.</p>
                             <p>Cras imperdiet in ligula sit amet fringilla. Morbi posuere mattis ornare. Nunc iaculis porta ipsum. Aliquam vehicula egestas diam auctor ornare.</p>
-                        </div>    
+                        </div>
                     </div>
                     <div class="col-md-6">
                         <div class="tm-about-1">
                             <img src="img/img-3x1-02.jpg" alt="Image" class="img-fluid mb-5">
                             <p class="tm-article-text">Quisque sed tortor sed metus vehicula pulvinar vel dictum tortor. Nivamus eu posuere arcu. Vivamus vel urna id mi congue convallis at sed augue.</p>
                             <p>Praesent venenatis risus aliquet semper porta. Maecenas mollis nisi non hendrerit finibus. Ut eu convallis massa. Sed imperdiet arcu non egestas elementum.</p>
-                        </div>    
+                        </div>
                     </div>
                 </div>
             </section>
-            
+
             <!-- Featured -->
             <section class="tm-featured">
                 <div class="row">
                     <div class="col-12">
-                        <h2 class="tm-section-header tm-section-header-small mb-3">Our Team Members</h2>        
+                        <h2 class="tm-section-header tm-section-header-small mb-3">Our Team Members</h2>
                     </div>
                 </div>
-                
+
                 <!-- Carousel -->
                 <div class="grid tm-carousel">
                     <figure class="effect-zoe">
@@ -147,10 +146,10 @@ https://templatemo.com/tm-542-new-vision
                         <figcaption>
                             <h2>John Stone</h2>
                             <p class="icon-links">
-								<a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
-								<a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-								<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
-							</p>
+                                <a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                                <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                            </p>
                         </figcaption>
                     </figure>
                     <figure class="effect-zoe">
@@ -158,10 +157,10 @@ https://templatemo.com/tm-542-new-vision
                         <figcaption>
                             <h2>Jane Meldrum</h2>
                             <p class="icon-links">
-								<a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
-								<a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-								<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
-							</p>
+                                <a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                                <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                            </p>
                         </figcaption>
                     </figure>
                     <figure class="effect-zoe">
@@ -169,10 +168,10 @@ https://templatemo.com/tm-542-new-vision
                         <figcaption>
                             <h2>Trevor Virtue</h2>
                             <p class="icon-links">
-								<a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
-								<a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-								<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
-							</p>
+                                <a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                                <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                            </p>
                         </figcaption>
                     </figure>
                     <figure class="effect-zoe">
@@ -180,10 +179,10 @@ https://templatemo.com/tm-542-new-vision
                         <figcaption>
                             <h2>Emily Moore</h2>
                             <p class="icon-links">
-								<a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
-								<a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-								<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
-							</p>
+                                <a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                                <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                            </p>
                         </figcaption>
                     </figure>
                     <figure class="effect-zoe">
@@ -191,10 +190,10 @@ https://templatemo.com/tm-542-new-vision
                         <figcaption>
                             <h2>Claire Li</h2>
                             <p class="icon-links">
-								<a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
-								<a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-								<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
-							</p>
+                                <a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                                <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                            </p>
                         </figcaption>
                     </figure>
                     <figure class="effect-zoe">
@@ -202,10 +201,10 @@ https://templatemo.com/tm-542-new-vision
                         <figcaption>
                             <h2>Elisabeth Shu</h2>
                             <p class="icon-links">
-								<a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
-								<a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-								<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
-							</p>
+                                <a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                                <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                            </p>
                         </figcaption>
                     </figure>
                     <figure class="effect-zoe">
@@ -213,10 +212,10 @@ https://templatemo.com/tm-542-new-vision
                         <figcaption>
                             <h2>Dania Liu</h2>
                             <p class="icon-links">
-								<a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
-								<a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-								<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
-							</p>
+                                <a href="https://fb.com"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                                <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                            </p>
                         </figcaption>
                     </figure>
                     <figure class="effect-zoe">
@@ -224,21 +223,21 @@ https://templatemo.com/tm-542-new-vision
                         <figcaption>
                             <h2>Miracle Yang</h2>
                             <p class="icon-links">
-								<a href="https://facebook.com"><i class="fab fa-facebook-f"></i></a>
-								<a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-								<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
-							</p>
+                                <a href="https://facebook.com"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                                <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+                            </p>
                         </figcaption>
                     </figure>
                 </div>
             </section>
-            
+
             <footer>
-                Copyright &copy; 2020 New Vision 
-                
+                Copyright &copy; 2020 New Vision
+
                 - Design: <a href="https://templatemo.com" rel="sponsored" target="_parent" title="css templates">TemplateMo</a>
             </footer>
-            
+
         </main>
     </div>
     <script src="{{asset('theme/js/jquery-3.4.1.min.js')}}"></script>
@@ -246,4 +245,5 @@ https://templatemo.com/tm-542-new-vision
     <script src="{{asset('theme/js/bootstrap.min.js')}}"></script>
     <script src="{{asset('theme/js/templatemo-script.js')}}"></script>
 </body>
+
 </html>
