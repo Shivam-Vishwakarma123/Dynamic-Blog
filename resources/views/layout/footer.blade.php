39ef4ac3c4b2cd332c025409c27c65213d10b553
Shivam-Vishwakarma123 <shiva83023@gmail.com>
2024-02-18 02:23:22 +0530

    Theme added

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    Copyright &copy; 2020 New Vision
+
+    - Design: <a href="https://templatemo.com" rel="sponsored" target="_parent" title="css templates">TemplateMo</a>
+</footer>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,44 @@
+<div class="tm-site-header">
+      <div class="row">
+        <div class="col-12 tm-site-header-col">
+          <div class="tm-site-header-left">
+            <i class="far fa-2x fa-eye tm-site-icon"></i>
+            <h1 class="tm-site-name">svBlogs</h1>
+          </div>
+          <div class="tm-site-header-right tm-menu-container-outer">
+
+            <!--Navbar-->
+            <nav class="navbar navbar-expand-lg">
+
+              <!-- Collapse button -->
+              <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i class="fas fa-bars fa-1x"></i></span></button>
+
+              <!-- Collapsible content -->
+              <div class="collapse navbar-collapse tm-nav" id="navbarSupportedContent1">
+
+                <!-- Links -->
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link tm-nav-link" href="{{asset('home')}}">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link tm-nav-link" href="{{asset('company')}}">Our Company</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link tm-nav-link" href="{{asset('services')}}">Services</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link tm-nav-link" href="{{asset('contact')}}">Contact</a>
+                  </li>
+                </ul>
+                <!-- Links -->
+
+              </div>
+              <!-- Collapsible content -->
+
+            </nav>
+            <!--/.Navbar-->
+          </div>
+        </div>
+      </div>
+    </div>
